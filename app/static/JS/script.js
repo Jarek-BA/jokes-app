a75e5c6c63a5a9546bb1353cb8be102af9b0198f
Jarek-BA <jaroslav.patak@outlook.com>
2025-09-07 15:17:56 +0200

    Wrapping the JS in DOMContentLoaded

--- app/static/JS/script.js
+++ app/static/JS/script.js
@@ -1,11 +1,14 @@
-document.getElementById("joke-btn").addEventListener("click", async () => {
-  try {
-    let res = await fetch("/joke");
-    if (!res.ok) throw new Error("Network response was not ok");
-    let data = await res.json();
-    document.getElementById("joke").innerText = data.joke;
-  } catch (err) {
-    document.getElementById("joke").innerText = "Failed to load joke.";
-    console.error(err);
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById("joke-btn").addEventListener("click", async () => {
+    try {
+      const res = await fetch("/joke");
+      if (!res.ok) throw new Error("Network response was not ok");
+      const data = await res.json();
+      document.getElementById("joke").innerText =
+        data.type === "single" ? data.joke : `${data.setup} ... ${data.delivery}`;
+    } catch (err) {
+      document.getElementById("joke").innerText = "Failed to load joke.";
+      console.error(err);
+    }
+  });
 });
