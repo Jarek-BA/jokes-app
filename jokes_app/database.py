092d2a9afb935262a8b2cafa41198c77326dfec3
Jarek-BA <jaroslav.patak@outlook.com>
2025-09-23 20:37:46 +0200

    Fixing test_main.py

--- jokes_app/database.py
+++ jokes_app/database.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///:memory:")
+DATABASE_URL = os.getenv("DATABASE_URL") or "sqlite+aiosqlite:///:memory:"
 
 # Async engine
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
