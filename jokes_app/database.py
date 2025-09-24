b0ac41dfe5b90c258194099b54027298bca511fd
Jarek-BA <jaroslav.patak@outlook.com>
2025-09-24 18:18:15 +0200

    Resolving the connection from Render to Postgres db

--- jokes_app/database.py
+++ jokes_app/database.py
@@ -5,18 +5,8 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-def get_database_url() -> str:
-    user = os.getenv("DB_USER", "postgres")
-    password = os.getenv("DB_PASSWORD", "")
-    host = os.getenv("DB_HOST", "localhost")
-    port = os.getenv("DB_PORT", "5432")
-    dbname = os.getenv("DB_NAME", "postgres")
-    use_ssl = os.getenv("DB_USE_SSL", "false").lower() in ("1", "true", "yes")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-    ssl_param = "require" if use_ssl else "disable"
-    return f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{dbname}?ssl={ssl_param}"
-
-DATABASE_URL = get_database_url()
 engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 async_session = async_sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
 
